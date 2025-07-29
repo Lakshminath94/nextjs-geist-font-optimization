6659bf114ca7e2f9398952554847ec77b0ed4261
Lakshminath94 <lakshminathreddy94@gmail.com>
2025-07-29 13:29:32 +0530

    Add .next/server/app/api/organisms/route.js 1753775975725

---
+++ .next/server/app/api/organisms/route.js
@@ -0,0 +1,7 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/organisms/route.js";
+const runtime = require("../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_next_379f3e69._.js");
+runtime.loadChunk("server/chunks/[root-of-the-server]__1b5cdbbb._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/organisms/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/organisms/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/organisms/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
